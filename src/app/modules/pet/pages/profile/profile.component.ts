46f0572f10733db0964f3f64ef5c60e4d8a3bd81
Luis Lucero <luis.lucero@bjungle.net>
2024-02-19 16:45:05 -0500

    feat profile

--- src/app/modules/pet/pages/profile/profile.component.ts
+++ src/app/modules/pet/pages/profile/profile.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {StorageService} from "../../../../core/services/storage/storage.service";
+import {Pet} from "../../../../core/models/pets";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public pet: Pet = {
+    age: 0,
+    category: '',
+    created_at: '',
+    id: 0,
+    name: '',
+    sexo: '',
+    type: '',
+    updated_at: '',
+    user: 0,
+    weight: 0,
+  };
 
+  constructor(
+    private _storageService: StorageService,
+  ) {
+
+  }
+
+  async ngOnInit() {
+    this.pet = await this._storageService.get('pet');
+  }
 }
